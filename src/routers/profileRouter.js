7c73031355d5a88ae2274cd4d4d0bb376001ce84
EldarMemetov <eldarm9347@gmail.com>
2025-11-08 20:03:51 +0100

    fix route all

--- src/routers/profileRouter.js
+++ src/routers/profileRouter.js
@@ -8,11 +8,12 @@ import upload from '../middlewares/uploadMiddleware.js';
 import * as mediaController from '../contacts/profileMediaController.js';
 const profileRouter = Router();
 
+profileRouter.get(
+  '/all',
+  ctrlWrapper(profileController.getAllProfilesController),
+);
 // Все руты требуют авторизации
 profileRouter.use(authenticate);
-
-// Получить свой профиль
-profileRouter.get('/', ctrlWrapper(profileController.getProfileController));
 
 // Обновить свой профиль
 profileRouter.patch(
@@ -27,11 +28,6 @@ profileRouter.get(
   ctrlWrapper(profileController.getProfileByIdController),
 );
 
-// Получить все профили
-profileRouter.get(
-  '/all',
-  ctrlWrapper(profileController.getAllProfilesController),
-);
 profileRouter.post(
   '/upload-photo',
   upload.single('photo'),
