92e5edc2b66f372acb17a6f6739df161ef7ef01c
EldarMemetov <eldarm9347@gmail.com>
2025-11-24 15:51:46 +0100

    fix pino

--- src/middlewares/logger.js
+++ src/middlewares/logger.js
@@ -1,9 +1,15 @@
-import pino from 'pino-http';
+import pinoHttp from 'pino-http';
+import { env } from '../utils/env.js';
 
-const logger = pino({
-  transport: {
-    target: 'pino-pretty',
-  },
+const isProduction = env('NODE_ENV', 'development') === 'production';
+
+const logger = pinoHttp({
+  transport: isProduction
+    ? undefined
+    : {
+        target: 'pino-pretty',
+        options: { colorize: true },
+      },
 });
 
 export default logger;
