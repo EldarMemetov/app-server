af0b7dd49db2980c39b5bfc1b8f10b74689de876
EldarMemetov <eldarm9347@gmail.com>
2025-11-25 19:15:35 +0100

    new fix test

--- src/routers/profileRouter.js
+++ src/routers/profileRouter.js
@@ -6,23 +6,43 @@ import * as profileController from '../contacts/profileController.js';
 import { userUpdateProfileSchema } from '../validation/users.js';
 import upload from '../middlewares/uploadMiddleware.js';
 import * as mediaController from '../contacts/profileMediaController.js';
+import { filterUsersController } from '../contacts/filterController.js';
 
-const router = Router();
+const profileRouter = Router();
 
-router.use(authenticate);
+profileRouter.get('/filter', ctrlWrapper(filterUsersController));
 
-router.get('/me', ctrlWrapper(profileController.getProfileController));
+profileRouter.get(
+  '/all',
+  ctrlWrapper(profileController.getAllProfilesController),
+);
 
-router.patch(
+// Все руты требуют авторизации
+profileRouter.use(authenticate);
+
+profileRouter.get(
+  '/me',
+  authenticate,
+  ctrlWrapper(profileController.getProfileController),
+);
+
+// Обновить свой профиль
+profileRouter.patch(
   '/',
   validateBody(userUpdateProfileSchema),
   ctrlWrapper(profileController.updateProfileController),
 );
 
-router.post(
+// Получить профиль любого пользователя по ID
+profileRouter.get(
+  '/:id',
+  ctrlWrapper(profileController.getProfileByIdController),
+);
+
+profileRouter.post(
   '/upload-photo',
   upload.single('photo'),
   ctrlWrapper(mediaController.uploadProfilePhotoController),
 );
 
-export default router;
+export default profileRouter;
