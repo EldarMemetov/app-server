0d98a3994acd1145f6798380ddb9686e5769af00
EldarMemetov <eldarm9347@gmail.com>
2025-12-01 22:03:23 +0100

    fix token

--- src/constants/users.js
+++ src/constants/users.js
@@ -2,4 +2,4 @@ export const emailRegexp = /\S+@\S+\.\S+/;
 
 export const accessTokenLifetime = 1000 * 60 * 15;
 
-export const refreshTokenLifetime = 1000 * 60 * 60 * 24;
+export const refreshTokenLifetime = 1000 * 60 * 60 * 24 * 30;
--- src/contacts/auth.js
+++ src/contacts/auth.js
@@ -1,3 +1,204 @@
+// import bcrypt from 'bcrypt';
+// import { randomBytes } from 'crypto';
+
+// import createHttpError from 'http-errors';
+// import UserCollection from '../db/models/User.js';
+// import SessionCollection from '../db/models/Session.js';
+
+// import jwt from 'jsonwebtoken';
+// import { TEMPLATES_DIR } from '../constants/index.js';
+// import { env } from '../utils/env.js';
+// import { sendEmail } from '../utils/sendEmail.js';
+// import handlebars from 'handlebars';
+// import path from 'node:path';
+// import fs from 'node:fs/promises';
+
+// import {
+//   accessTokenLifetime,
+//   refreshTokenLifetime,
+// } from '../constants/users.js';
+// import { validateCode } from '../utils/googleOAuth.js';
+
+// const createSession = () => {
+//   const accessToken = randomBytes(30).toString('base64');
+//   const refreshToken = randomBytes(30).toString('base64');
+
+//   const accessTokenValidUntil = new Date(Date.now() + accessTokenLifetime);
+//   const refreshTokenValidUntil = new Date(Date.now() + refreshTokenLifetime);
+
+//   return {
+//     accessToken,
+//     refreshToken,
+//     accessTokenValidUntil,
+//     refreshTokenValidUntil,
+//   };
+// };
+
+// export const signup = async (payload) => {
+//   const { email, password } = payload;
+//   const user = await UserCollection.findOne({ email });
+//   if (user) {
+//     throw createHttpError(409, 'Email already exist');
+//   }
+
+//   const hashPassword = await bcrypt.hash(password, 10);
+
+//   const data = await UserCollection.create({
+//     ...payload,
+//     password: hashPassword,
+//   });
+//   delete data._doc.password;
+//   return data._doc;
+// };
+
+// export const signin = async (payload) => {
+//   const { email, password } = payload;
+//   const user = await UserCollection.findOne({ email });
+//   if (!user) {
+//     throw createHttpError(401, 'Email or password invalid');
+//   }
+
+//   await SessionCollection.deleteOne({ userId: user._id });
+
+//   const passwordCompare = await bcrypt.compare(password, user.password);
+//   if (!passwordCompare) {
+//     throw createHttpError(401, 'Email or password invalid ');
+//   }
+//   const sessionData = createSession();
+
+//   const userSession = await SessionCollection.create({
+//     userId: user._id,
+//     ...sessionData,
+//   });
+
+//   return userSession;
+// };
+
+// export const signinOrSignupWitGoogleOAuth = async (code) => {
+//   const loginTicket = await validateCode(code);
+//   const payload = loginTicket.getPayload();
+
+//   let user = await UserCollection.findOne({ email: payload.email });
+//   if (!user) {
+//     const password = randomBytes(10);
+//     const hashPassword = await bcrypt.hash(password, 10);
+//     user = await UserCollection.create({
+//       email: payload.email,
+//       name: payload.name,
+//       password: hashPassword,
+//       verify: true,
+//     });
+//     delete user._doc.password;
+//   }
+
+//   const sessionData = createSession();
+
+//   const userSession = await SessionCollection.create({
+//     userId: user._id,
+//     ...sessionData,
+//   });
+
+//   return userSession;
+// };
+
+// export const findSessionByAccessToken = (accessToken) =>
+//   SessionCollection.findOne({ accessToken });
+
+// export const refreshSession = async ({ refreshToken, sessionId }) => {
+//   const oldSession = await SessionCollection.findOne({
+//     _id: sessionId,
+//     refreshToken,
+//   });
+//   if (!oldSession) {
+//     throw createHttpError(401, 'Session not found');
+//   }
+//   if (new Date() > oldSession.refreshTokenValidUntil) {
+//     throw createHttpError(401, 'Session token expired');
+//   }
+
+//   await SessionCollection.deleteOne({ _id: sessionId });
+
+//   const sessionData = createSession();
+
+//   const userSession = await SessionCollection.create({
+//     userId: oldSession.userId,
+//     ...sessionData,
+//   });
+
+//   return userSession;
+// };
+
+// export const signout = async (sessionId) => {
+//   await SessionCollection.deleteOne({ _id: sessionId });
+// };
+
+// export const findUser = (filter) => UserCollection.findOne(filter);
+
+// export const requestResetToken = async (email) => {
+//   const user = await UserCollection.findOne({ email });
+//   if (!user) {
+//     throw createHttpError(404, 'User not found');
+//   }
+
+//   const resetToken = jwt.sign(
+//     {
+//       sub: user._id,
+//       email,
+//     },
+//     env('JWT_SECRET'),
+//     { expiresIn: '15m' },
+//   );
+
+//   const resetPasswordTemplatePath = path.join(
+//     TEMPLATES_DIR,
+//     'reset-password-email.html',
+//   );
+//   const templateSource = (
+//     await fs.readFile(resetPasswordTemplatePath)
+//   ).toString();
+//   const template = handlebars.compile(templateSource);
+
+//   const html = template({
+//     name: user.name,
+//     link: `${env('APP_DOMAIN')}/reset-password?token=${resetToken}`,
+//   });
+
+//   await sendEmail({
+//     from: env('SMTP_FROM'),
+//     to: email,
+//     subject: 'Reset your password',
+//     html,
+//   });
+// };
+
+// export const resetPassword = async (payload) => {
+//   let decodedToken;
+
+//   try {
+//     decodedToken = jwt.verify(payload.token, env('JWT_SECRET'));
+//   } catch (err) {
+//     if (err instanceof Error)
+//       throw createHttpError(401, 'Invalid or expired token');
+//     throw err;
+//   }
+
+//   const user = await UserCollection.findOne({
+//     email: decodedToken.email,
+//     _id: decodedToken.sub,
+//   });
+
+//   if (!user) {
+//     throw createHttpError(404, 'User not found');
+//   }
+
+//   const encryptedPassword = await bcrypt.hash(payload.password, 10);
+
+//   await UserCollection.updateOne(
+//     { _id: user._id },
+//     { password: encryptedPassword },
+//   );
+// };
+// contacts/auth.js
 import bcrypt from 'bcrypt';
 import { randomBytes } from 'crypto';
 
@@ -18,6 +219,17 @@ import {
   refreshTokenLifetime,
 } from '../constants/users.js';
 import { validateCode } from '../utils/googleOAuth.js';
+
+const cleanupExpiredSessions = async (userId) => {
+  try {
+    await SessionCollection.deleteMany({
+      userId,
+      refreshTokenValidUntil: { $lt: new Date() },
+    });
+  } catch (err) {
+    console.error('Failed to cleanup sessions', err);
+  }
+};
 
 const createSession = () => {
   const accessToken = randomBytes(30).toString('base64');
@@ -58,12 +270,13 @@ export const signin = async (payload) => {
     throw createHttpError(401, 'Email or password invalid');
   }
 
-  await SessionCollection.deleteOne({ userId: user._id });
+  await cleanupExpiredSessions(user._id);
 
   const passwordCompare = await bcrypt.compare(password, user.password);
   if (!passwordCompare) {
     throw createHttpError(401, 'Email or password invalid ');
   }
+
   const sessionData = createSession();
 
   const userSession = await SessionCollection.create({
@@ -90,6 +303,8 @@ export const signinOrSignupWitGoogleOAuth = async (code) => {
     });
     delete user._doc.password;
   }
+
+  await cleanupExpiredSessions(user._id);
 
   const sessionData = createSession();
 
