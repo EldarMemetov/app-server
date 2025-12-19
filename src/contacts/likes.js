ecc0a4ba358fb2ff965fb11e0cf7cce52811a58c
EldarMemetov <eldarm9347@gmail.com>
2025-12-19 23:41:33 +0100

    fix socket

--- src/contacts/likes.js
+++ src/contacts/likes.js
@@ -1,4 +1,96 @@
-// contacts/likes.js
+// import LikeCollection from '../db/models/like.js';
+// import UserCollection from '../db/models/User.js';
+// import createHttpError from 'http-errors';
+// import mongoose from 'mongoose';
+
+// export const getLikesCount = async (toUserId) => {
+//   const user = await UserCollection.findById(toUserId).select('likesCount');
+//   return user ? user.likesCount ?? 0 : 0;
+// };
+
+// export const isLikedBy = async (fromUserId, toUserId) => {
+//   if (!fromUserId) return false;
+//   const doc = await LikeCollection.findOne({ fromUserId, toUserId });
+//   return !!doc;
+// };
+
+// export const likeUser = async (fromUserId, toUserId, io = null) => {
+//   if (!mongoose.Types.ObjectId.isValid(toUserId)) {
+//     throw createHttpError(400, 'Invalid target id');
+//   }
+//   if (fromUserId.toString() === toUserId.toString()) {
+//     throw createHttpError(400, 'Нельзя лайкать себя');
+//   }
+
+//   try {
+//     const exists = await LikeCollection.findOne({ fromUserId, toUserId });
+
+//     if (exists) {
+//       const likesCount = await getLikesCount(toUserId);
+//       return { liked: true, likesCount };
+//     }
+
+//     await LikeCollection.create({ fromUserId, toUserId });
+
+//     await UserCollection.findByIdAndUpdate(toUserId, {
+//       $inc: { likesCount: 1 },
+//     });
+
+//     if (io?.userSockets) {
+//       const set = io.userSockets.get(String(toUserId));
+//       if (set) {
+//         for (const s of set) {
+//           s.emit('likeUpdate', { toUserId: String(toUserId), liked: true });
+//         }
+//       }
+//     }
+
+//     const likesCount = await getLikesCount(toUserId);
+//     return { liked: true, likesCount };
+//   } catch (err) {
+//     console.error('likeUser error', err);
+
+//     throw err;
+//   }
+// };
+
+// export const unlikeUser = async (fromUserId, toUserId, io = null) => {
+//   if (!mongoose.Types.ObjectId.isValid(toUserId)) {
+//     throw createHttpError(400, 'Invalid target id');
+//   }
+
+//   try {
+//     const deleted = await LikeCollection.findOneAndDelete({
+//       fromUserId,
+//       toUserId,
+//     });
+
+//     if (!deleted) {
+//       const likesCount = await getLikesCount(toUserId);
+//       return { removed: false, liked: false, likesCount };
+//     }
+
+//     await UserCollection.findByIdAndUpdate(toUserId, {
+//       $inc: { likesCount: -1 },
+//     });
+
+//     if (io?.userSockets) {
+//       const set = io.userSockets.get(String(toUserId));
+//       if (set) {
+//         for (const s of set) {
+//           s.emit('likeUpdate', { toUserId: String(toUserId), liked: false });
+//         }
+//       }
+//     }
+
+//     const likesCount = await getLikesCount(toUserId);
+//     return { removed: true, liked: false, likesCount };
+//   } catch (err) {
+//     console.error('unlikeUser error', err);
+//     throw err;
+//   }
+// };
+
 import LikeCollection from '../db/models/like.js';
 import UserCollection from '../db/models/User.js';
 import createHttpError from 'http-errors';
@@ -7,12 +99,6 @@ import mongoose from 'mongoose';
 export const getLikesCount = async (toUserId) => {
   const user = await UserCollection.findById(toUserId).select('likesCount');
   return user ? user.likesCount ?? 0 : 0;
-};
-
-export const isLikedBy = async (fromUserId, toUserId) => {
-  if (!fromUserId) return false;
-  const doc = await LikeCollection.findOne({ fromUserId, toUserId });
-  return !!doc;
 };
 
 export const likeUser = async (fromUserId, toUserId, io = null) => {
@@ -28,29 +114,61 @@ export const likeUser = async (fromUserId, toUserId, io = null) => {
 
     if (exists) {
       const likesCount = await getLikesCount(toUserId);
+
+      if (io?.userSockets) {
+        const set = io.userSockets.get(String(toUserId));
+        if (set) {
+          for (const s of set) {
+            s.emit('likeUpdate', {
+              toUserId: String(toUserId),
+              liked: true,
+              likesCount,
+            });
+          }
+        }
+
+        io.emit('likeUpdate', {
+          toUserId: String(toUserId),
+          liked: true,
+          likesCount,
+        });
+      }
       return { liked: true, likesCount };
     }
 
     await LikeCollection.create({ fromUserId, toUserId });
 
-    await UserCollection.findByIdAndUpdate(toUserId, {
-      $inc: { likesCount: 1 },
-    });
+    const updatedUser = await UserCollection.findByIdAndUpdate(
+      toUserId,
+      { $inc: { likesCount: 1 } },
+      { new: true },
+    );
+
+    const likesCount =
+      updatedUser?.likesCount ?? (await getLikesCount(toUserId));
 
     if (io?.userSockets) {
       const set = io.userSockets.get(String(toUserId));
       if (set) {
         for (const s of set) {
-          s.emit('likeUpdate', { toUserId: String(toUserId), liked: true });
+          s.emit('likeUpdate', {
+            toUserId: String(toUserId),
+            liked: true,
+            likesCount,
+          });
         }
       }
+
+      io.emit('likeUpdate', {
+        toUserId: String(toUserId),
+        liked: true,
+        likesCount,
+      });
     }
 
-    const likesCount = await getLikesCount(toUserId);
     return { liked: true, likesCount };
   } catch (err) {
     console.error('likeUser error', err);
-
     throw err;
   }
 };
@@ -68,23 +186,43 @@ export const unlikeUser = async (fromUserId, toUserId, io = null) => {
 
     if (!deleted) {
       const likesCount = await getLikesCount(toUserId);
+      if (io?.userSockets) {
+        io.emit('likeUpdate', {
+          toUserId: String(toUserId),
+          liked: false,
+          likesCount,
+        });
+      }
       return { removed: false, liked: false, likesCount };
     }
 
-    await UserCollection.findByIdAndUpdate(toUserId, {
-      $inc: { likesCount: -1 },
-    });
+    const updatedUser = await UserCollection.findByIdAndUpdate(
+      toUserId,
+      { $inc: { likesCount: -1 } },
+      { new: true },
+    );
+
+    const likesCount =
+      updatedUser?.likesCount ?? (await getLikesCount(toUserId));
 
     if (io?.userSockets) {
       const set = io.userSockets.get(String(toUserId));
       if (set) {
         for (const s of set) {
-          s.emit('likeUpdate', { toUserId: String(toUserId), liked: false });
+          s.emit('likeUpdate', {
+            toUserId: String(toUserId),
+            liked: false,
+            likesCount,
+          });
         }
       }
+      io.emit('likeUpdate', {
+        toUserId: String(toUserId),
+        liked: false,
+        likesCount,
+      });
     }
 
-    const likesCount = await getLikesCount(toUserId);
     return { removed: true, liked: false, likesCount };
   } catch (err) {
     console.error('unlikeUser error', err);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,72 @@
+// import express from 'express';
+// import cors from 'cors';
+// import helmet from 'helmet';
+// import cookieParser from 'cookie-parser';
+// import http from 'http';
+// import { Server } from 'socket.io';
+// import { env } from './utils/env.js';
+
+// import logger from './middlewares/logger.js';
+// import notFoundHandler from './middlewares/notFoundHandler.js';
+// import errorHandler from './middlewares/errorHandler.js';
+
+// import authRouter from './routers/auth.js';
+// import profileRouter from './routers/profileRouter.js';
+// import postsRouter from './routers/postsRouter.js';
+// import moderatorRouter from './routers/moderatorRouter.js';
+// import portfolioRouter from './routers/portfolioRouter.js';
+// import locationRouter from './routers/locationRouter.js';
+// import notificationsRouter from './routers/notificationsRouter.js';
+// import profilePublicRouter from './routers/profilePublicRouter.js';
+
+// import corsOptions from './utils/corsOptions.js';
+// import { initSocket } from './socket/socket.js';
+
+// export const setupServer = () => {
+//   const app = express();
+//   const port = Number(env('PORT', 3000));
+
+//   app.use(helmet());
+//   app.use(logger);
+//   app.use(cors(corsOptions));
+//   app.use(express.json());
+//   app.use(cookieParser());
+
+//   app.get('/', (req, res) => {
+//     res.send({
+//       message: 'Qvrix API is running 🚀',
+//       status: 'OK',
+//       timestamp: new Date().toISOString(),
+//     });
+//   });
+
+//   app.use('/auth', authRouter);
+//   app.use('/profile', profileRouter);
+//   app.use('/people', profilePublicRouter);
+//   app.use('/posts', postsRouter);
+//   app.use('/moderation', moderatorRouter);
+//   app.use('/portfolio', portfolioRouter);
+//   app.use('/location', locationRouter);
+//   app.use('/notifications', notificationsRouter);
+
+//   app.use(notFoundHandler);
+//   app.use(errorHandler);
+
+//   const server = http.createServer(app);
+
+//   const io = new Server(server, {
+//     cors: {
+//       origin: true,
+//       credentials: true,
+//     },
+//   });
+
+//   initSocket(io);
+
+//   server.listen(port, () => {
+//     console.log(`🚀 Server running on port ${port}`);
+//   });
+// };
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
@@ -60,6 +129,8 @@ export const setupServer = () => {
       credentials: true,
     },
   });
+
+  app.set('io', io);
 
   initSocket(io);
 
