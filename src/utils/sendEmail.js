3842be0d2cc7d44cf5d6b8a8b430afea0cb5459b
EldarMemetov <eldarm9347@gmail.com>
2025-12-22 20:56:56 +0100

    test

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,17 +1,43 @@
-import nodemailer from 'nodemailer';
+// import nodemailer from 'nodemailer';
 
+// import { SMTP } from '../constants/index.js';
+// import { env } from '../utils/env.js';
+
+// const transporter = nodemailer.createTransport({
+//   host: env(SMTP.SMTP_HOST),
+//   port: Number(env(SMTP.SMTP_PORT)),
+//   auth: {
+//     user: env(SMTP.SMTP_USER),
+//     pass: env(SMTP.SMTP_PASSWORD),
+//   },
+// });
+
+// export const sendEmail = async (options) => {
+//   return await transporter.sendMail(options);
+// };
+import nodemailer from 'nodemailer';
 import { SMTP } from '../constants/index.js';
 import { env } from '../utils/env.js';
 
 const transporter = nodemailer.createTransport({
   host: env(SMTP.SMTP_HOST),
   port: Number(env(SMTP.SMTP_PORT)),
+  secure: false,
+  requireTLS: true,
   auth: {
     user: env(SMTP.SMTP_USER),
     pass: env(SMTP.SMTP_PASSWORD),
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
-export const sendEmail = async (options) => {
-  return await transporter.sendMail(options);
+export const sendEmail = async ({ to, subject, html }) => {
+  return transporter.sendMail({
+    from: env(SMTP.SMTP_FROM),
+    to,
+    subject,
+    html,
+  });
 };
