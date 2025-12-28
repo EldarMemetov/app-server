7ec5cac6bc561fcb325caf61c136bfcf49dedcc2
EldarMemetov <eldarm9347@gmail.com>
2025-12-28 20:25:06 +0000

    fix

--- src/routers/favorites.js
+++ src/routers/favorites.js
@@ -6,6 +6,6 @@ import ctrlWrapper from '../utils/ctrlWrapper.js';
 
 const router = Router();
 
-router.patch('/toggle', authenticate, toggleFavoriteController);
+router.patch('/toggle', authenticate, ctrlWrapper, toggleFavoriteController);
 router.get('/', authenticate, ctrlWrapper(getMyFavoritesController));
 export default router;
