26a339f3acaa1bef82fe7cc963c76a9bfbdc2b6a
EldarMemetov <eldarm9347@gmail.com>
2025-12-30 21:25:29 +0000

    add comentar

--- src/middlewares/uploadMiddleware.js
+++ src/middlewares/uploadMiddleware.js
@@ -1,24 +1,3 @@
-// import multer from 'multer';
-// import path from 'path';
-// import { fileURLToPath } from 'url';
-
-// const __dirname = path.dirname(fileURLToPath(import.meta.url));
-// const tempDir = path.join(__dirname, '../../tmp');
-
-// const storage = multer.diskStorage({
-//   destination: tempDir,
-//   filename: (_, file, cb) => {
-//     const uniquePrefix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-//     const ext = path.extname(file.originalname);
-//     cb(null, `${uniquePrefix}${ext}`);
-//   },
-// });
-
-// const upload = multer({ storage });
-
-// export default upload;
-// uploadMiddleware.js
-// uploadMiddleware.js
 import multer from 'multer';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -36,9 +15,7 @@ const storage = multer.diskStorage({
   },
 });
 
-const GLOBAL_MAX_BYTES = Number(
-  env('GLOBAL_MAX_BYTES', 120 * 1024 * 1024), // 120MB
-);
+const GLOBAL_MAX_BYTES = Number(env('GLOBAL_MAX_BYTES', 120 * 1024 * 1024));
 
 const fileFilter = (req, file, cb) => {
   if (!file.mimetype) return cb(null, false);
